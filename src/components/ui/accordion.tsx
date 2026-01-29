2753d17f2a63ecc7120f3f38ceb37405e232adf5
Uniquebeing <beingunique049@gmail.com>
2026-01-29 16:29:15 +0100

    Update accordion.tsx

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -10,7 +10,7 @@ const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
 >(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item ref={ref} className={cn("border-b", className)} {...props} />
+  <AccordionPrimitive.Item ref={ref} className={cn("border-b border-muted", className)} {...props} />
 ));
 AccordionItem.displayName = "AccordionItem";
 
@@ -22,7 +22,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between py-4 font-semibold transition-all hover:text-accent [&[data-state=open]>svg]:rotate-180",
         className,
       )}
       {...props}
@@ -43,7 +43,7 @@ const AccordionContent = React.forwardRef<
     className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    <div className={cn("pb-4 pt-1 text-muted-foreground", className)}>{children}</div>
   </AccordionPrimitive.Content>
 ));
 
