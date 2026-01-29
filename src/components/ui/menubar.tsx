76e87fb6005485f89b47ac35df7cece1e906b83a
Uniquebeing <beingunique049@gmail.com>
2026-01-29 17:03:28 +0100

    Update menubar.tsx

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -185,7 +185,7 @@ MenubarSeparator.displayName = MenubarPrimitive.Separator.displayName;
 const MenubarShortcut = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) => {
   return <span className={cn("ml-auto text-xs tracking-widest text-muted-foreground", className)} {...props} />;
 };
-MenubarShortcut.displayname = "MenubarShortcut";
+MenubarShortcut.displayName = "MenubarShortcut";
 
 export {
   Menubar,
