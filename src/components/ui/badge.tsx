6bc266db42ab17635781572f0bf702f2c9b9fe2e
Uniquebeing <beingunique049@gmail.com>
2026-01-30 15:48:13 +0100

    Add BadgeProps interface to badge component

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -25,6 +25,7 @@ const badgeVariants = cva(
   },
 );
 
+
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
