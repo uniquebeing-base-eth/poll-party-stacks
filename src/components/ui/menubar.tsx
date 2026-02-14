a031ade32769e439938f3e6d2ef15fc2968628f8
Uniquebeing <beingunique049@gmail.com>
2026-02-14 15:31:30 +0100

    Update menubar.tsx

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -6,6 +6,7 @@ import { Check, ChevronRight, Circle } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
+
 const MenubarMenu = MenubarPrimitive.Menu;
 
 const MenubarGroup = MenubarPrimitive.Group;
