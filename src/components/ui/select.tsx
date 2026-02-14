3ce07c2c567d4872dd0ae133b67a19b488cd507a
Uniquebeing <beingunique049@gmail.com>
2026-02-14 15:37:40 +0100

    Update select.tsx

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -6,6 +6,7 @@ import { Check, ChevronDown, ChevronUp } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
+
 const Select = SelectPrimitive.Root;
 
 const SelectGroup = SelectPrimitive.Group;
