ff12fb6cbc6dbd42c30dacd9806b019501be4a63
Uniquebeing <beingunique049@gmail.com>
2026-02-15 00:20:40 +0100

    Update sonner.tsx

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -6,6 +6,7 @@ import { Toaster as Sonner, toast } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
+
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
 
