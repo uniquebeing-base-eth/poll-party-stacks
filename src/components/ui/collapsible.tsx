5c612c529850d70d659243f239e726906e1f7913
Uniquebeing <beingunique049@gmail.com>
2026-02-14 15:26:40 +0100

    Update collapsible.tsx

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -3,6 +3,7 @@
 
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
 
+
 const Collapsible = CollapsiblePrimitive.Root;
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
